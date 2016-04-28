d3095b601cfca46d086d5c2c61279dfb5ea634ad
Badichan Dmitriy <badimalex@gmail.com>
2016-04-28 11:11:22 +0000

    rewrite test with shoulda-matchers

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,24 +2,7 @@ require 'rails_helper'
 
 describe Answer do
   it { should belong_to(:question) }
-
   it { should validate_presence_of(:question_id) }
-
-  it 'is invalid without a body' do
-    answer = Answer.new(body: nil)
-    expect(answer).to be_invalid
-  end
-
-  it 'is invalid when body < 10' do
-    answer = Answer.new(body: 'lorem')
-    answer.valid?
-    expect(answer.errors[:body].size).to eq(1)
-  end
-
-  it 'is valid with a question_id and body' do
-    answer = Answer.new(
-      question_id: 1,
-      body: 'Lorem Ipsum is simply dummy text of the printing')
-    expect(answer).to be_valid
-  end
+  it { should validate_presence_of(:body) }
+  it { should validate_length_of(:body).is_at_least(10) }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,34 +1,9 @@
 require 'rails_helper'
 
 describe Question do
-  it { should have_many(:answers) }
-
-  it 'is invalid without a title' do
-    question = Question.new(title: nil)
-    expect(question).to be_invalid
-  end
-
-  it 'is invalid when title < 5' do
-    question = Question.new(title: 'min')
-    question.valid?
-    expect(question.errors[:title].size).to eq(1)
-  end
-
-  it 'is invalid without a body' do
-    question = Question.new(body: nil)
-    expect(question).to be_invalid
-  end
-
-  it 'is invalid when body < 15' do
-    question = Question.new(body: 'lorem ipsum')
-    question.valid?
-    expect(question.errors[:body].size).to eq(1)
-  end
-
-  it 'is valid with a title and body' do
-    question = Question.new(
-      title: 'Question',
-      body: 'Lorem Ipsum is simply dummy text of the printing')
-    expect(question).to be_valid
-  end
+  it { should have_many(:answers).dependent(:destroy) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(15) }
 end
