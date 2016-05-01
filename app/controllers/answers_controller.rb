1570d6159cd6a3c5f4cc34c22d728f4a879873bf
Badichan Dmitriy <badimalex@gmail.com>
2016-05-01 19:18:01 +0000

    added test for answers_controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,26 @@
 class AnswersController < ApplicationController
+  before_action :load_question
+
   def new
     @answer = Answer.new
+  end
+
+  def create
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
+  def load_question
+    @question = Question.find(params[:question_id])
   end
 end
