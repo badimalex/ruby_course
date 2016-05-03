5bcb2d62ced6a9cbdc2e56269fc463b2324314e1
Badichan Dmitriy <badimalex@gmail.com>
2016-05-03 11:20:27 +0000

    remove dublicate test

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,18 +2,6 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
-
-  describe 'GET #new' do
-    before { get :new, question_id: question.id }
-
-    it 'assigns a new answer to @answer' do
-      expect(assigns(:answer)).to be_a_new(Answer)
-    end
-    
-    it 'renders new view' do
-      expect(response).to render_template :new
-    end
-  end
   
   describe 'POST #new' do
     context 'with valid attributes' do
