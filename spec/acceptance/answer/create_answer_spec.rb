7b48e148b7cb35158793bdd0ee8944d7904a8035
Badichan Dmitriy <badimalex@gmail.com>
2016-05-05 11:53:54 +0000

    added test for answer question

---
+++ spec/acceptance/answer/create_answer_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'Answer a question' do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  scenario 'User answer a question' do
+    User.create!(email: 'user@test.com', password: '12345678')
+    sign_in user
+
+    visit question_path
+    click_on 'Add answer'
+    fill_in 'Body', with: 'My awesome answer body'
+    click_on 'Send'
+
+    expect(page).to have_content 'My awesome answer body'
+  end
+end
