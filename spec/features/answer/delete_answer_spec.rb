a7da531b42f5fad2720b01d299516454dfa0732b
Badichan Dmitriy <badimalex@gmail.com>
2016-05-06 19:35:15 +0000

    added acceptance test for user delete answer

--- spec/features/answer/delete_answer_spec.rb
+++ spec/features/answer/delete_answer_spec.rb
@@ -7,9 +7,12 @@ feature 'Delete answer' do
 
   scenario 'Author deletes own answer' do
     sign_in user
+
     visit question_path(question)
     click_on 'Remove answer'
-  
-    
+
+    expect(page).to have_content 'Your answer successfully removed'
+    expect(page).to_not have_content(answer.body)
+    expect(current_path).to eq question_path(question)
   end
 end
