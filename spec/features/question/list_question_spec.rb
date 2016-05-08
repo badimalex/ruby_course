7075c5eb583a1a25f13ad85eb426cda145f8e734
Badichan Dmitriy <badimalex@gmail.com>
2016-05-08 07:15:12 +0000

    added test for check empty list of questions

--- spec/features/question/list_question_spec.rb
+++ spec/features/question/list_question_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 feature 'List question' do
-  given!(:questions) { create_list(:question, 5) }
+  given(:questions) { create_list(:question, 5) }
 
   scenario 'User can view list of questions' do
+    questions
     visit questions_path
+
     questions.each do |question|
       expect(page).to have_link(question.title)
     end
+  end
+
+  scenario 'User can view empty list of questions' do
+    visit questions_path
+      expect(page).to have_content('Is no questions')
   end
 end
 # здесь только один сценарий, еще какие то могут быть?
