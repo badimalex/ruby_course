fb15f206fc603667e9c552b1c68185cbe7c8d24a
Badichan Dmitriy <badimalex@gmail.com>
2016-05-10 18:06:04 +0000

    some changes

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -9,29 +9,29 @@ RSpec.describe AnswersController, type: :controller do
     sign_in_user
     context 'with valid attributes' do
       it 'saves the new answer in database' do
-        expect { post :create, question_id: question.id, answer: attributes_for(:answer) }
+        expect { post :create, question_id: question, answer: attributes_for(:answer) }
           .to change(question.answers, :count).by(1) && change(@user.answers, :count).by(1)
       end
 
       it 'associates answer with question' do
-        expect { post :create, question_id: question.id, answer: attributes_for(:answer) }
+        expect { post :create, question_id: question, answer: attributes_for(:answer) }
           .to change(question.answers, :count).by(1)
       end
 
-      it 'redirects to question page' do
-        post :create, question_id: question.id, answer: attributes_for(:answer)
+      it 'redirects to question show view' do
+        post :create, question_id: question, answer: attributes_for(:answer)
         expect(response).to redirect_to question_path(question)
       end
     end
 
     context 'with invalid attributes' do
       it 'does not save answer' do
-        expect { post :create, question_id: question.id, answer: { body: nil } }
+        expect { post :create, question_id: question, answer: { body: nil } }
           .to_not change(Answer, :count)
       end
 
       it 're-render new view' do
-        post :create, question_id: question.id, answer: { body: nil }
+        post :create, question_id: question, answer: { body: nil }
         expect(response).to render_template 'questions/show'
       end
     end
