6b6ba4d247673b448a081a4a81cd80b554506f5f
Badichan Dmitriy <badimalex@gmail.com>
2016-05-16 22:22:00 +0300

    acceptance validate edit question

--- spec/features/question/edit_question_spec.rb
+++ spec/features/question/edit_question_spec.rb
@@ -18,11 +18,30 @@ feature 'Question editing' do
       sign_in user
       visit question_path(question)
     end
+
     scenario 'sees link to edit question' do
       within '.question' do
         expect(page).to have_link('Edit question')
       end
     end
+
+    scenario 'try to edit question with ivalid data', js: true do
+      within '.question' do
+        click_on 'Edit question'
+        fill_in 'Title', with: nil
+        fill_in 'Body', with: nil
+        click_on 'Save question'
+        expect(page).to have_content question.body
+        expect(page).to have_selector 'textarea'
+        within '.question-errors' do
+          expect(page).to have_content 'Title can\'t be blank'
+          expect(page).to have_content 'Title is too short (minimum is 5 characters)'
+          expect(page).to have_content 'Body can\'t be blank'
+          expect(page).to have_content 'Body is too short (minimum is 15 characters)'
+        end
+      end
+    end
+
     scenario 'try to edit question', js: true do
       within '.question' do
         click_on 'Edit question'
