519aef14cb11985783c90c700dc17c3273053b1b
Badichan Dmitriy <badimalex@gmail.com>
2016-05-21 22:41:04 +0300

    user cant remove other user file (test controller)

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,6 +1,10 @@
 class AttachmentsController < ApplicationController
   def destroy
     @attachment = Attachment.find(params[:id])
-    @attachment.destroy
+    if current_user.author_of?(@attachment.attachmentable)
+      @attachment.destroy
+    else
+      render status: :forbidden
+    end
   end
 end
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe AttachmentsController, type: :controller do
-  # let(:question) { create(:question) }
+  sign_in_user
+  let!(:question) { create(:question, user: @user) }
+  let!(:other_question) { create(:question, user: create(:user)) }
   let!(:attachment) { create(:attachment) }
+  let!(:other_attachment) { create(:attachment) }
 
   describe 'Delete #destroy' do
     context 'Author deletes own answer attachment file' do
+      before do
+        question.attachments.push attachment
+      end
       it 'assign attachment to @attachment' do
         delete :destroy, id: attachment, format: :js
         expect(assigns(:attachment)).to eq attachment
@@ -13,6 +19,20 @@ RSpec.describe AttachmentsController, type: :controller do
 
       it 'deletes file' do
         expect { delete :destroy, id: attachment, format: :js }.to change(Attachment, :count).by(-1)
+      end
+    end
+
+    context 'Author delete other author file' do
+      before do
+        other_question.attachments.push other_attachment
+      end
+      it 'doesn\'t deletes file' do
+        expect { delete :destroy, id: other_attachment, format: :js }.to_not change(Attachment, :count)
+      end
+
+      it 'stay at current_path' do
+        delete :destroy, id: other_attachment, format: :js
+        expect(response).to have_http_status(:forbidden)
       end
     end
   end
