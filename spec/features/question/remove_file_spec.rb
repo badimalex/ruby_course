1c54eecfc50b4254b5ddff33f935ddc2c8fe19b7
Badichan Dmitriy <badimalex@gmail.com>
2016-05-21 20:28:18 +0300

    user cant view link to remove other user files

--- spec/features/question/remove_file_spec.rb
+++ spec/features/question/remove_file_spec.rb
@@ -5,6 +5,8 @@ feature 'Add files to question' do
   given(:question) { create(:question, user: user) }
   given(:attachment) { create(:attachment) }
 
+  given(:other_question) { create(:question, user: create(:user)) }
+  given(:other_attachment) { create(:attachment) }
 
   scenario 'User remove file from your question', js: true do
     sign_in user
@@ -15,6 +17,16 @@ feature 'Add files to question' do
       expect(page).to have_content('spec_helper')
       click_on 'Remove attachment'
       expect(page).to_not have_content('spec_helper')
+    end
+  end
+
+  scenario 'User try to remove other user file', js: true do
+    sign_in user
+    other_question.attachments.push other_attachment
+    visit question_path(other_question)
+    within '.attachments' do
+      expect(page).to have_content('spec_helper')
+      expect(page).to_not have_link('Remove attachment')
     end
   end
 end
