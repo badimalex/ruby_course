937d3d7ceb4f48a681c444e5c02c85bd8cc3960a
Badichan Dmitriy <badimalex@gmail.com>
2016-05-21 23:34:39 +0300

    user delete files from answer (acceptance, controller tests)

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,6 +1,7 @@
 class AttachmentsController < ApplicationController
   def destroy
     @attachment = Attachment.find(params[:id])
+    puts(@attachment.attachmentable.inspect)
     if current_user.author_of?(@attachment.attachmentable)
       @attachment.destroy
     else
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -3,15 +3,18 @@ require 'rails_helper'
 RSpec.describe AttachmentsController, type: :controller do
   sign_in_user
   let!(:question) { create(:question, user: @user) }
+  let!(:answer) { create(:answer, user: @user, question: question) }
   let!(:other_question) { create(:question, user: create(:user)) }
   let!(:attachment) { create(:attachment) }
   let!(:other_attachment) { create(:attachment) }
+  let!(:other_answer) { create(:answer, user: create(:user), question: other_question) }
 
   describe 'Delete #destroy' do
-    context 'Author deletes own answer attachment file' do
+    context 'Author deletes own question file' do
       before do
         question.attachments.push attachment
       end
+
       it 'assign attachment to @attachment' do
         delete :destroy, id: attachment, format: :js
         expect(assigns(:attachment)).to eq attachment
@@ -22,17 +25,38 @@ RSpec.describe AttachmentsController, type: :controller do
       end
     end
 
-    context 'Author delete other author file' do
+    context 'Author delete other author question file' do
       before do
         other_question.attachments.push other_attachment
       end
-      it 'doesn\'t deletes file' do
+
+      it 'doesnt deletes file' do
         expect { delete :destroy, id: other_attachment, format: :js }.to_not change(Attachment, :count)
       end
 
       it 'stay at current_path' do
         delete :destroy, id: other_attachment, format: :js
         expect(response).to have_http_status(:forbidden)
+      end
+    end
+
+    context 'Author delete own answer file' do
+      before do
+        answer.attachments.push attachment
+      end
+
+      it 'deletes file' do
+        expect { delete :destroy, id: attachment, format: :js }.to change(Attachment, :count).by(-1)
+      end
+    end
+
+    context 'Author delete other answer file' do
+      before do
+        other_answer.attachments.push other_attachment
+      end
+
+      it 'doesnt deletes file' do
+        expect { delete :destroy, id: other_attachment, format: :js }.to_not change(Attachment, :count)
       end
     end
   end
