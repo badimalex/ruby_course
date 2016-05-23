1e116245f7ef6bce040e3400915c830bb191a674
Badichan Dmitriy <badimalex@gmail.com>
2016-05-23 23:21:28 +0300

    remove debug line

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,7 +1,6 @@
 class AttachmentsController < ApplicationController
   def destroy
     @attachment = Attachment.find(params[:id])
-    puts(@attachment.attachmentable.inspect)
     if current_user.author_of?(@attachment.attachmentable)
       @attachment.destroy
     else
