43faf56c5da5234bd99c8890653b869454c306e0
Badichan Dmitriy <badimalex@gmail.com>
2016-05-26 23:58:33 +0300

    revert

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,10 +3,9 @@ class Question < ActiveRecord::Base
   has_many :attachments, as: :attachmentable
   belongs_to :user
 
-  validates :title, :body, :user_id, :votes, presence: true
+  validates :title, :body, :user_id, presence: true
   validates :title, length: { minimum: 5 }
   validates :body, length: { minimum: 15 }
-  validates_numericality_of :votes
 
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
 end
--- db/migrate/20160526124212_add_votes_field_to_question.rb
+++
@@ -1,6 +0,0 @@
-class AddVotesFieldToQuestion < ActiveRecord::Migration
-  def change
-    add_column :questions, :votes, :integer, default: 0
-    add_index :questions, :votes
-  end
-end
