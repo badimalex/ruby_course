0bd1d0ceb51f254c8d4fb7198ebae91e11fbd713
Badichan Dmitriy <badimalex@gmail.com>
2016-05-28 00:40:16 +0300

    add score column to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,8 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   belongs_to :user
 
-  validates :title, :body, :user_id, presence: true
+  validates :title, :body, :user_id, :score, presence: true
   validates :title, length: { minimum: 5 }
   validates :body, length: { minimum: 15 }
+  validates_numericality_of :score
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,9 +8,11 @@ describe Question do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
   it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:score) }
 
   it { should validate_length_of(:title).is_at_least(5) }
   it { should validate_length_of(:body).is_at_least(15) }
+  it { should validate_numericality_of(:score) }
 
   it { should accept_nested_attributes_for :attachments }
 end
