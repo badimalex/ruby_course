f4df3cba0259533377f269a560d637f18a287d51
Badichan Dmitriy <badimalex@gmail.com>
2016-05-28 09:58:00 +0300

    change polymorphic concern

--- app/controllers/concerns/voted.rb
+++ app/controllers/concerns/voted.rb
@@ -1,13 +1,21 @@
 module Voted
   extend ActiveSupport::Concern
 
+  included do
+    before_action :set_votable, only: [:upvote]
+  end
+
   def upvote
-    @question.increment!(:score, 1)
+    @votable.increment!(:score, 1)
   end
 
   private
 
   def model_klass
     controller_name.classify.constantize
+  end
+
+  def set_votable
+    @votable = model_klass.find(params[:id])
   end
 end
