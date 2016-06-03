7a2c8cba1c0b908ef5416ed9482b66371acc44da
Badichan Dmitriy <badimalex@gmail.com>
2016-06-03 14:38:00 +0300

    Fix question rating

--- spec/features/question/rating_question_spec.rb
+++ spec/features/question/rating_question_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 feature 'Viewing question rating' do
-  given(:question) { create(:question, up_votes: 5, down_votes: 3) }
+  given(:question) { create(:question, up_votes: 4, down_votes: 1) }
 
   scenario 'User can view question rating' do
     visit question_path(question)
 
     within '.question>.vote' do
-        expect(find('.rating')).to have_content '2'
+      expect(find('.rating')).to have_content '3'
     end
   end
 end
