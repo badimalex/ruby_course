d282415d6d9332a5f552d7e85012b34e7c2fa39e
Badichan Dmitriy <badimalex@gmail.com>
2016-06-20 15:59:09 +0300

    Test when user has authorization

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe OmniauthCallbacksController do
+  OmniAuth.config.test_mode = true
+
   describe 'GET #twitter' do
-    context 'user already has authorization'
+    context 'user already has authorization' do
+      before do
+        OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(provider: 'twitter', uid: '123456', info: { email: 'confirmed@email.com' })
+        set_request
+        get :twitter
+      end
+
+      it { response.should redirect_to root_path }
+    end
+
     context 'user has not authorization' do
       before do
-        set_omniauth
+        OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(provider: 'twitter', uid: '123456', info: { })
         set_request
         get :twitter
       end
