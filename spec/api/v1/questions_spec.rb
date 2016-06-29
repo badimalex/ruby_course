2d5d2767a7b69a4d4dcbe1e896628125d7aa431f
Badichan Dmitriy <badimalex@gmail.com>
2016-06-29 16:02:47 +0300

    Added api for Questions#show

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -92,5 +92,17 @@ describe 'Questions API' do
         expect(response.body).to be_json_eql(attachment.file.url.to_json).at_path("attachments/0/file/url")
       end
     end
+
+    context 'answers' do
+      it 'included in question object' do
+        expect(response.body).to have_json_size(1).at_path("0/answers")
+      end
+
+      %w(id body created_at updated_at).each do |attr|
+        it "answer object contains #{attr}" do
+          expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("0/answers/0/#{attr}")
+        end
+      end
+    end
   end
 end
