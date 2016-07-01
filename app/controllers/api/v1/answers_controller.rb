207a3b0a98963ca4f4ce732af410707a0b082ddd
Badichan Dmitriy <badimalex@gmail.com>
2016-07-01 10:59:04 +0300

    Answer/show api

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,5 +1,22 @@
 class Api::V1::AnswersController < Api::V1::BaseController
+  before_action :load_question, only: [:index]
+  before_action :load_answer, only: [:show]
+
   def index
-    render nothing: true
+    respond_with @question.answers
+  end
+
+  def show
+    respond_with @answer
+  end
+
+  private
+
+  def load_answer
+    @answer = Answer.find(params[:id])
+  end
+
+  def load_question
+    @question = Question.find(params[:question_id])
   end
 end
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -31,4 +31,61 @@ describe 'Answers API' do
       end
     end
   end
+
+  describe 'GET /show' do
+    context 'unauthorized' do
+      let(:question) { create(:question) }
+      let!(:answer) { create(:answer, question: question) }
+      it 'returns 401 status if there is no access_token' do
+        get "/api/v1/questions/#{question.id}/answers/#{answer.id}", format: :json
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get "/api/v1/questions/#{question.id}/answers/#{answer.id}", format: :json, access_token: '1234'
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token) }
+      let!(:question) { create(:question) }
+      let!(:answer) { create(:answer, question: question) }
+      let!(:attachment) { create(:attachment, attachmentable: answer) }
+      let!(:comment) { create(:comment, commentable: answer) }
+      before { get "/api/v1/questions/#{question.id}/answers/#{answer.id}", format: :json, access_token: access_token.token }
+
+      it 'returns 200 status code' do
+        expect(response).to be_success
+      end
+
+      %w(id body created_at updated_at).each do |attr|
+        it "answer object contains #{attr}" do
+          expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("#{attr}")
+        end
+      end
+
+      context 'attachments' do
+        it 'included in answer object' do
+          expect(response.body).to have_json_size(1).at_path("attachments")
+        end
+
+        it 'attachment object contain file url' do
+          expect(response.body).to be_json_eql(attachment.file.url.to_json).at_path("attachments/0/file/url")
+        end
+      end
+
+      context 'comments' do
+        it 'included in answer object' do
+          expect(response.body).to have_json_size(1).at_path("comments")
+        end
+
+        %w(id body created_at updated_at).each do |attr|
+          it "comment object contains #{attr}" do
+            expect(response.body).to be_json_eql(comment.send(attr.to_sym).to_json).at_path("comments/0/#{attr}")
+          end
+        end
+      end
+    end
+  end
 end
