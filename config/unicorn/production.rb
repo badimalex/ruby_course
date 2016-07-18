a90431421637512e513143d40ee507cd0a76a7f3
Badichan Dmitriy <badimalex@gmail.com>
2016-07-18 12:21:08 +0300

    fix path

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,5 +1,5 @@
 # paths
-app_path = "/home/deployer/ruby_course/current"
+app_path = "/home/deployer/ruby_course"
 working_directory "#{app_path}/current"
 pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 
